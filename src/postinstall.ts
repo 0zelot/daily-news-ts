efeafff57025374a016a570c6a9c864dcdb79875
0zelot <ozelot7331@gmail.com>
2020-11-18 13:34:02 +0100

    Changes in package.json and fixes with postinstall.

---
+++ src/postinstall.ts
@@ -0,0 +1,6 @@
+const colors = require("colors");
+
+console.log(
+    colors.yellow(`\n[DAILY-NEWS-TS] `) + colors.green(`Thanks for using my library. See readme for help.\n`) +
+    colors.yellow(`[DAILY-NEWS-TS] `) + colors.green(`https://github.com/0zelot/daily-news-ts\n`)
+);
