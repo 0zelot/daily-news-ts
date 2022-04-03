9b49811362fb8c97e24d4f568afccb00fe7073e9
unknown <ozelot@ctemplar.com>
2022-04-03 10:32:59 +0200

    Changed website url

--- src/postinstall.ts
+++ src/postinstall.ts
@@ -3,5 +3,5 @@ const colors = require("colors");
 console.log(
     colors.yellow(`\n[DAILY-NEWS-TS] `) + colors.green(`Thanks for using my library. See readme for help.\n`) +
     colors.yellow(`[DAILY-NEWS-TS] `) + colors.green(`https://github.com/0zelot/daily-news-ts\n`) +
-    colors.yellow(`[DAILY-NEWS-TS] `) + colors.green(`https://open-api.klonba.ml\n`)
+    colors.yellow(`[DAILY-NEWS-TS] `) + colors.green(`https://open-api.klonba.xyz\n`)
 );
